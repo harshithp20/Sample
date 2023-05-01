13f074eee690e5844542200a03c2d6fc58566140
harshithp20 <harshithp20@gmail.com>
2023-05-01 13:54:31 +0530

    1st commit

---
+++ src/test/java/practice/ParametersTest.java
@@ -0,0 +1,27 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ParametersTest {
+	@Test(groups="smokeTest")
+	public void m1()
+	{
+		WebDriver driver = null;
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			driver=WebDriverManager.chromedriver().create();
+		}else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			driver=WebDriverManager.firefoxdriver().create();
+		}
+		driver.get(URL);
+	
+	}
+
+}
+
---
+++ src/test/java/practice/SampleTest.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test(groups="regressionTest")
+	public void test()
+	{
+		System.out.println("Method running");
+	}
+
+}
